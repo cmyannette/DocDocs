6d3beac8ae5de0e76c8586c246f44f47b45d55b2
cmyannette <132402329+cmyannette@users.noreply.github.com>
2024-09-28 17:20:34 -0500

    made filepath global variable in main

--- DocDocs/Backend/BackendMain.cs
+++ DocDocs/Backend/BackendMain.cs
@@ -1,9 +1,12 @@
 public static class BackendMain {
+
+    private const string filePath = ".\\Patient_Provider_Demo.m4a";
+
     public static async Task Main(string[] args)
         {
             // Get call recording from Zoom Cloud
             
-            string filePath = ".\\Patient_Provider_Demo.m4a";
+            
 
             // Upload File to AWS S3 Bucket
             await AWSFactory.UploadFileAsync(filePath);
