4f1e92cbb26442743e6063e2e01edc2825c368ef
Caleb Hans <caleb.hansolo@gmail.com>
2024-09-29 05:15:19 -0500

    fixed main i HATE GIT SO MUCH UAHSUOLHAOD JAOSUHFIUA H

--- DocDocs/Frontend/src/components/Home/HomePage/HomePage.js
+++ DocDocs/Frontend/src/components/Home/HomePage/HomePage.js
@@ -3,7 +3,6 @@ import './HomePage.css';
 
 function HomePage({ patient, meetings, onJoinMeeting }) {
   return (
-<<<<<<< HEAD
     <div className="main-content">
       <section id="home">
         <div className="hero-image">
@@ -22,20 +21,6 @@ function HomePage({ patient, meetings, onJoinMeeting }) {
         <p className="meeting-info">
           You have <span className="upcoming-meetings">{meetings > 0 ? meetings : 'no'}</span> upcoming meetings.
         </p>
-=======
-    <div class="main-content">
-      <section id="home">
-      <div class="hero-image">
-        <div class="hero-text" id="hero-text">
-            <h1 id="hero-text">Welcome to <b>DocDocs</b></h1>
-            <h3 id="hero-text">Your telehealth transcription assistant! </h3>
-        </div>
-      </div>
-      </section>
-      <section id="meetings">
-        <h2>Meetings</h2>
-        <p>Hello {patient.name}. You have {meetings > 0 ? meetings : 'no'} upcoming meetings.</p>
->>>>>>> Feature-AWS
         {/* Update the Join Meeting button */}
         {meetings > 0 && (
           <button className="join-meeting-btn" onClick={onJoinMeeting}>
